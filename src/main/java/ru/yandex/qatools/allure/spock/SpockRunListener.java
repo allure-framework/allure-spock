d41950126887080c1ab5f6811f9e59b319262e61
Maksym Bryzhko <maksym@transferwise.com>
2014-10-09 22:07:05 +0300

    remove wildcard import

--- src/main/java/ru/yandex/qatools/allure/spock/SpockRunListener.java
+++ src/main/java/ru/yandex/qatools/allure/spock/SpockRunListener.java
@@ -7,7 +7,11 @@ import org.spockframework.runtime.model.FeatureInfo;
 import org.spockframework.runtime.model.IterationInfo;
 import org.spockframework.runtime.model.SpecInfo;
 import ru.yandex.qatools.allure.Allure;
-import ru.yandex.qatools.allure.events.*;
+import ru.yandex.qatools.allure.events.TestCaseFinishedEvent;
+import ru.yandex.qatools.allure.events.TestCaseFailureEvent;
+import ru.yandex.qatools.allure.events.TestCaseStartedEvent;
+import ru.yandex.qatools.allure.events.TestSuiteFinishedEvent;
+import ru.yandex.qatools.allure.events.TestSuiteStartedEvent;
 import ru.yandex.qatools.allure.utils.AnnotationManager;
 import spock.lang.Unroll;
 
